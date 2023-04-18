49cdccaffe55969d5474f6ba90067c24ad13a88b
Matthew Hefferon <matthefferon@gmail.com>
2023-04-18 15:59:01 -0600

    Prettier formatting and had ChatGPT add tooltips

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useRef } from 'react';
-import * as d3 from 'd3';
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
 
 const data = [
-  { name: 'A', value: 10 },
-  { name: 'B', value: 20 },
-  { name: 'C', value: 30 },
-  { name: 'D', value: 40 },
+  { name: "A", value: 10 },
+  { name: "B", value: 20 },
+  { name: "C", value: 30 },
+  { name: "D", value: 40 },
 ];
 
 const BarChart = () => {
@@ -15,8 +15,8 @@ const BarChart = () => {
     const svg = d3.select(chartRef.current);
 
     const margin = { top: 20, right: 20, bottom: 30, left: 40 };
-    const width = +svg.attr('width') - margin.left - margin.right;
-    const height = +svg.attr('height') - margin.top - margin.bottom;
+    const width = +svg.attr("width") - margin.left - margin.right;
+    const height = +svg.attr("height") - margin.top - margin.bottom;
 
     const xScale = d3
       .scaleBand()
@@ -33,26 +33,46 @@ const BarChart = () => {
     const yAxis = d3.axisLeft(yScale);
 
     svg
-      .append('g')
-      .attr('transform', `translate(${margin.left}, ${height + margin.top})`)
+      .append("g")
+      .attr("transform", `translate(${margin.left}, ${height + margin.top})`)
       .call(xAxis);
 
     svg
-      .append('g')
-      .attr('transform', `translate(${margin.left}, ${margin.top})`)
+      .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`)
       .call(yAxis);
 
+    const tooltip = d3
+      .select("body")
+      .append("div")
+      .attr("class", "tooltip")
+      .style("position", "absolute")
+      .style("padding", "10px")
+      .style("background-color", "white")
+      .style("border", "1px solid black")
+      .style("opacity", 0);
+
     svg
-      .selectAll('.bar')
+      .selectAll(".bar")
       .data(data)
       .enter()
-      .append('rect')
-      .attr('class', 'bar')
-      .attr('x', (d) => xScale(d.name) + margin.left)
-      .attr('y', (d) => yScale(d.value) + margin.top)
-      .attr('width', xScale.bandwidth())
-      .attr('height', (d) => height - yScale(d.value))
-      .attr('fill', 'steelblue');
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", (d) => xScale(d.name) + margin.left)
+      .attr("y", (d) => yScale(d.value) + margin.top)
+      .attr("width", xScale.bandwidth())
+      .attr("height", (d) => height - yScale(d.value))
+      .attr("fill", "steelblue")
+      .on("mouseover", (event, d) => {
+        tooltip.transition().duration(200).style("opacity", 0.9);
+        tooltip
+          .html(`Name: ${d.name}<br/>Value: ${d.value}`)
+          .style("left", `${event.pageX}px`)
+          .style("top", `${event.pageY}px`);
+      })
+      .on("mouseout", () => {
+        tooltip.transition().duration(500).style("opacity", 0);
+      });
   }, []);
 
   return (
